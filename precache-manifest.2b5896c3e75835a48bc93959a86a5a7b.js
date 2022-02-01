fb565b695ec65e8c08df98a697746e0f5d4e3cf9
ddaniel90 <ddaniel.andrei@outlook.com>
2022-02-01 11:01:47 +0200

    Updates

---
+++ precache-manifest.2b5896c3e75835a48bc93959a86a5a7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b781388515b32af83ec33e407ee544",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "caafbe230f4e7d2edd84",
+    "url": "/monsters-rolodex/static/css/main.fda953a5.chunk.css"
+  },
+  {
+    "revision": "01240f3c4ba6e7bc9b70",
+    "url": "/monsters-rolodex/static/js/2.c4a2e0fe.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/monsters-rolodex/static/js/2.c4a2e0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48a9730b00b42723b60",
+    "url": "/monsters-rolodex/static/js/3.1119309c.chunk.js"
+  },
+  {
+    "revision": "caafbe230f4e7d2edd84",
+    "url": "/monsters-rolodex/static/js/main.d1e3dc9c.chunk.js"
+  },
+  {
+    "revision": "305fc5360722635b1f2c",
+    "url": "/monsters-rolodex/static/js/runtime-main.b3b2c6c5.js"
+  }
+]);
